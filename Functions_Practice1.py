c60499d3f7495fa4fa8a67171b180c415456a920
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-04-29 06:46:43 +0700

    update Functions_Practice.py and Function_Practice1.py

--- Functions_Practice1.py
+++ Functions_Practice1.py
@@ -1,3 +1,5 @@
+# Create a function named in_range() that has three parameters named num, lower, and upper.
+# The function should return True if num is greater than or equal to lower and less than or equal to upper. Otherwise, return False.
 # Write your in_range function here:
 def in_range(num, lower, upper):
   if num >= lower and num <= upper:
@@ -9,6 +11,8 @@ def in_range(num, lower, upper):
 #print(in_range(5, 10, 20))
 # should print False
 
+# Create a function named same_name() that has two parameters named your_name and my_name.
+# If our names are identical, return True. Otherwise, return False.
 # Write your same_name function here:
 def same_name(your_name, my_name):
   if your_name == my_name:
@@ -20,6 +24,9 @@ def same_name(your_name, my_name):
 #print(same_name("Tina", "Amber"))
 # should print False
 
+# Create a function named always_false() that has one parameter named num.
+# Using an if statement, your variable num, and the operators >, and <, make it so your function will return False no matter what number is stored in num.
+# An if statement that is always false is called a contradiction. You will rarely want to do this while programming, but it is important to realize it is possible to do this.
 # Write your always_false function here:
 def always_false(num):
   if num == num:
@@ -33,6 +40,8 @@ def always_false(num):
 #print(always_false(1))
 # should print False
 
+# Create a function named movie_review() that has one parameter named rating
+# If rating is less than or equal to 5, return "Avoid at all costs!". If rating is between 5 and 9, return "This one was fun.". If rating is 9 or above, return "Outstanding!"
 # Write your movie_review function here:
 def movie_review(rating):
   if rating <= 5:
@@ -50,6 +59,8 @@ def movie_review(rating):
 #print(movie_review(6))
 # should print "This one was fun."
 
+# Create a function called max_num() that has three parameters named num1, num2, and num3.
+# The function should return the largest of these three numbers. If any of two numbers tie as the largest, you should return "It's a tie!".
 # Write your max_num function here:
 def max_num(num1, num2, num3):
   if num1 > num2 and num1 > num3:
