d6d73152c8dd728c993545e6642c837ec561455e
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-09 23:33:51 +0700

    update

--- Dictionary.py
+++ Dictionary.py
@@ -9,3 +9,7 @@ sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
 num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
 
 print(sensors)
+
+# each key has its own value, no two keys have the same value ==> override value
+dictionary = {"key_1": "value_1", "key_1": "value_2", "key_2": "value_2"}
+print(dictionary) # {'key_1': 'value_2', 'key_2': 'value_2'}
