bbded47e29ac493365d5d1de847ff06ccc8b2b50
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-11 00:24:05 +0700

    update Dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -143,3 +143,15 @@ weather = { key:value for key,value in zip(cities, temps)}
 
 print(weather)
 # {'New York': 99, 'London': 81, 'Sydney': 65}
+
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+plays = {song:playcount for (song,playcount) in zip(songs, playcounts)}
+print(plays)
+plays["Purple Haze"] = 1
+plays.update({"Respect" : 94})
+
+library = {"The Best Songs" : plays, "Sunday Feelings" : {}}
+
+print(library)
