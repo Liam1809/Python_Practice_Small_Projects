e579bc4edc09e7d816919baca61dabef1e2ada4b
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-11 23:03:05 +0700

    values() method

--- Dictionary.py
+++ Dictionary.py
@@ -263,3 +263,31 @@ num_exercises.update({"exception":3})
 print(lessons)
 print(lesson_lst)
 
+# Get All Values
+# Dictionaries have a .values() method that returns a dict_values object (just like a dict_keys object but for values!) with all of the values in the dictionary. It can be used in the place of a list for iteration:
+
+# test_scores = {"Grace":[80, 72, 90], "Jeffrey":[88, 68, 81], "Sylvia":[80, 82, 84], "Pedro":[98, 96, 95], "Martin":[78, 80, 78], "Dina":[64, 60, 75]}
+
+# for score_list in test_scores.values():
+#   print(score_list)
+# will yield:
+
+# [80, 72, 90]
+# [88, 68, 81]
+# [80, 82, 84]
+# [98, 96, 95]
+# [78, 80, 78]
+# [64, 60, 75]
+# There is no built-in function to get all of the values as a list, but if you really want to, you can use:
+
+# list(test_scores.values())
+# However, for most purposes, the dict_list object will act the way you want a list to act.
+
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+for number in num_exercises.values():
+     total_exercises += number
+
+print(total_exercises)
+print(list(num_exercises.values()))
