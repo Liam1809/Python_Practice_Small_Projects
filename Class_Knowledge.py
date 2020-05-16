9813e6a9f1e22793f606b2749faa90118a79828a
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-17 00:49:28 +0700

    Methods with Arguments

--- Class_Knowledge.py
+++ Class_Knowledge.py
@@ -88,3 +88,17 @@
 # Above we created a Dog class with a time_explanation method that takes one argument, self, which refers to the object calling the function. We created a Dog named pipi_pitbull and called the .time_explanation() method on our new object for Pipi.
 
 # Notice we didn’t pass any arguments when we called .time_explanation(), but were able to refer to self in the function body. When you call a method it automatically passes the object calling the method as the first argument.
+
+# Methods with Arguments
+# Methods can also take more arguments than just self:
+
+# class DistanceConverter:
+#   kms_in_a_mile = 1.609
+#   def how_many_kms(self, miles):
+#     return miles * self.kms_in_a_mile
+
+# converter = DistanceConverter()
+# kms_in_5_miles = converter.how_many_kms(5)
+# print(kms_in_5_miles)
+# # prints "8.045"
+# Above we defined a DistanceConverter class, instantiated it, and used it to convert 5 miles into kilometers. Notice again that even though how_many_kms takes two arguments in its definition, we only pass miles, because self is implicitly passed (and refers to the object converter).
