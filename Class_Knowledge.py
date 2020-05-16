c6196434bfcf70c2559a87d4169fa686e2c75048
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-17 00:32:04 +0700

    Class Variables

--- Class_Knowledge.py
+++ Class_Knowledge.py
@@ -49,9 +49,24 @@
 
 # In Python __main__ means “this current file that we’re running” and so one could read the output from type() to mean “the class CoolClass that was defined here, in the script you’re currently running.”
 
-class Facade:
-      pass
+# class Facade:
+#       pass
 
-facade_1 = Facade()
-facade_1_type = type(facade_1)
-print(facade_1_type)
+# facade_1 = Facade()
+# facade_1_type = type(facade_1)
+# print(facade_1_type)
+
+# Class Variables
+# When we want the same data to be available to every instance of a class we use a class variable. A class variable is a variable that’s the same for every instance of the class.
+
+# You can define a class variable by including it in the indented part of your class definition, and you can access all of an object’s class variables with object.variable syntax.
+
+# class Musician:
+#   title = "Rockstar"
+
+# drummer = Musician()
+# print(drummer.title)
+# # prints "Rockstar"
+# Above we defined the class Musician, then instantiated drummer to be an object of type Musician. We then printed out the drummer’s .title attribute, which is a class variable that we defined as the string “Rockstar”.
+
+# If we defined another musician, like guitarist = Musician() they would have the same .title attribute.
