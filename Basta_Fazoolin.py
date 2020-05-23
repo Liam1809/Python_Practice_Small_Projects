181b631bf7b1571c14a5ab0a9f096620e3c83e3a
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-23 22:14:22 +0700

    Making Menus and orders

--- Basta_Fazoolin.py
+++ Basta_Fazoolin.py
@@ -5,15 +5,24 @@
 # Making the Menus
 # At Basta Fazoolin’ with my Heart our motto is simple: when you’re here with family, that’s great! We have four different menus: brunch, early-bird, dinner, and kids.
 class Menu():
+  # constructor
   def __init__(self, name, items, start_time, end_time):
     self.name = name
     self.items = items
     self.start_time = start_time
     self.end_time = end_time
+  # string repr
   def __repr__(self):
     return "{name} menu available from {start_time} to {end_time}".format(name = self.name, start_time = self.start_time, end_time = self.end_time)
 
-  
+  def calculate_bill(self, purchased_items):
+    total = 0
+    for key,value in purchased_items.items():
+      if key in self.items:
+        total += value * self.items[key]
+    return "Total Price is {total}$".format(total = total)
+
+# Making menus
 brunch_dict = {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }
@@ -27,13 +36,32 @@ kids_dict = {
   'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
 }
 
-brunch = Menu("brunch", brunch_dict, "11am", "4pm")
-early_bird = Menu("early_bird", early_bird_dict, "3pm", "6pm")
-dinner = Menu("dinner", dinner_dict, "5pm", "11pm")
-kids = Menu("kids", kids_dict, "11am", "9pm")
+# making instantiated of class Menu
+brunch = Menu("brunch", brunch_dict, 1100, 1600)
+early_bird = Menu("early_bird", early_bird_dict, 1500, 1800)
+dinner = Menu("dinner", dinner_dict, 1700, 2300)
+kids = Menu("kids", kids_dict, 1100, 2100)
 
+# display menus
+print("- Menus schedule")
 print(brunch)
 print(early_bird)
 print(dinner)
 print(kids)
+
+# Brunch order
+print("- Brunch order")
+brunch_order = {"pancakes" : 1, "home fries" : 1, "coffee" : 2}
+for key, value in brunch_order.items():
+  print("{order} order of {dish}".format(order = value, dish = key))
+# total price for Brunch order
+print(brunch.calculate_bill(brunch_order))
+
+# early-bird order
+print("- Early-bird order")
+early_bird_order = {"salumeria plate" : 3, "mushroom ravioli (vegan)": 2}
+for key, value in early_bird_order.items():
+  print("{order} order of {dish}".format(order = value, dish = key))
+# total price for early-bird order
+print(early_bird.calculate_bill(early_bird_order))
 
