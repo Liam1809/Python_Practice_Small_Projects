8f4882c481dfc8a8ce3bf5d1bdd672cae5fbbaad
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-25 21:39:00 +0700

    practice Using Both Keyword and Positional Unpacking

--- Argument_Parameter_Knowledge.py
+++ Argument_Parameter_Knowledge.py
@@ -309,3 +309,61 @@ print(myjoin(path_segment_1, path_segment_2, path_segment_3))
 # As you can see, **kwargs gives us a dictionary with all the keyword arguments that were passed to arbitrary_keyword_args. We can access these arguments using standard dictionary methods.
 
 # Since we’re not sure whether a keyword argument will exist, it’s probably best to use the dictionary’s .get() method to safely retrieve the keyword argument. Do you remember what .get() returns if the key is not in the dictionary? It’s None!
+
+
+# Using Both Keyword and Positional Unpacking
+# This keyword argument unpacking syntax can be used even if the function takes other parameters. However, the parameters must be listed in this order:
+
+# All named positional parameters
+# An unpacked positional parameter (*args)
+# All named keyword parameters
+# An unpacked keyword parameter (**kwargs)
+# Here’s a function with all possible types of parameter:
+
+# def main(filename, *args, user_list=None, **kwargs):
+#   if user_list is None:
+#     user_list = []
+
+#   if '-a' in args:
+#     user_list = all_users()
+
+#   if kwargs.get('active'):
+#     user_list = [user for user_list if user.active]
+
+#   with open(filename) as user_file:
+#     user_file.write(user_list)
+# Looking at the signature of main() we define four different kinds of parameters. The first, filename is a normal required positional parameter. The second, *args, is all positional arguments given to a function after that organized as a tuple in the parameter args. The third, user_list, is a keyword parameter with a default value. Lastly, **kwargs is all other keyword arguments assembled as a dictionary in the parameter kwargs.
+
+# A possible call to the function could look like this:
+
+# main("files/users/userslist.txt", 
+#      "-d", 
+#      "-a", 
+#      save_all_records=True, 
+#      user_list=current_users)
+# In the body of main() these arguments would look like this:
+
+# filename == "files/users/userslist.txt"
+# args == ('-d', '-a)
+# user_list == current_users
+# kwargs == {'save_all_records': True}
+# We can use all four of these kinds of parameters to create functions that handle a lot of possible arguments being passed to them.
+
+# the named (i.e., default) keyword argument and the **kwargs arguments can be interchanged in the function call. The only hard-and-fast rule is positional before named. But the interpreter is smart enough to sort out the named parameter from the keys in the **kwargs parameters.
+
+
+#  the function remove() has three parameters: the required positional filename, the arbitrary positional args, and the arbitrary keyword kwargs.
+def remove(filename, *args, **kwargs):
+  with open(filename) as file_obj:
+    text = file_obj.read()
+
+  # Add code here to update text.
+    for arg in args:
+      text = text.replace(arg, "")
+    for kwarg, replacement in kwargs.items():
+        text = text.replace(kwarg, replacement)
+    
+
+  return text
+
+print(remove("text.txt", "generous", "gallant", fond="amused by", Robin="Mr. Robin"))
