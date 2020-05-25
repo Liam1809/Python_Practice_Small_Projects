16928f9185b4ba8470653e2a10afacf74e494f24
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-25 15:28:53 +0700

    Using None as a Sentinel

--- Argument_Parameter_Knowledge.py
+++ Argument_Parameter_Knowledge.py
@@ -207,3 +207,15 @@ order2 = update_order({'item': 'soda', 'cost': '1.50'})
 
 # What's in that second order again?
 print(order2)
+
+
+# Using None as a Sentinel
+# So if we can’t use a list as a default argument for a function, what can we use? If we want an empty list, we can use None as a special value to indicate we did not receive anything. After we check whether an argument was provided we can instantiate a new list if it wasn’t.
+
+# def add_author(authors_books, current_books=None):
+#   if current_books is None:
+#     current_books = []
+
+#   current_books.extend(authors_books)
+#   return current_books
+# In the above function, we accept current_books a value expected to be a list. But we don’t require it. If someone calls add_author() without giving an argument for current_books, we supply an empty list. This way multiple calls to add_author won’t include data from previous calls to add_author.
