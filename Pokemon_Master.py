40391c29727e5e5337ea488ae98de6f04170586b
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-30 23:35:49 +0700

    create a pokemon

--- Pokemon_Master.py
+++ Pokemon_Master.py
@@ -1,3 +1,15 @@
 # Project Goals
 
 # In this project, you will be using Python Classes to create a game system similar to the popular game series Pokémon. If you’re unfamiliar with Pokémon, it is a game where creatures (Pokémon) battle against each other. Every Pokémon has statistics associated with it like health, level, type, and a name. In this project we’ll make several classes that interact with each other so you can create your own Pokémon battles!
+
+# This project invloved Classes and Inheritance properties
+
+class Pokemon:
+    # To create a pokemon, give it a name, type, and level. Its max health is determined by its level. Its starting health is its max health and it is not knocked out when it starts.
+    def __init__(self, name, type, level = 5):
+        self.name = name
+        self.level = level
+        self.health = level * 5
+        self.max_health = level * 5
+        self.type = type
+        self.is_knocked_out = False
