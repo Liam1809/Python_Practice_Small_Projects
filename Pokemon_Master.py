c54015a64e4684eb7e9b0988d2cd08f6742efa0e
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-30 23:39:42 +0700

    Methods for a pokemon revive(), knock_out(), lose_health(), gain_health()

--- Pokemon_Master.py
+++ Pokemon_Master.py
@@ -13,3 +13,44 @@ class Pokemon:
         self.max_health = level * 5
         self.type = type
         self.is_knocked_out = False
+        
+    def __repr__(self):
+        # Printing a pokemon will tell you its name, its type, its level and how much health it has remaining
+        return "This level {level} {name} has {health} hit points remaining. They are a {type} type Pokemon".format(level = self.level, name = self.name, health=self.health, type = self.type)
+
+    def revive(self):
+        # Reviving a pokemon will flip it's status to False
+        self.is_knocked_out = False
+        # A revived pokemon can't have 0 health. This is a safety precaution. revive() should only be called if the pokemon was given some health, but if it somehow has no health, its health gets set to 1.
+        if self.health == 0:
+            self.health = 1
+        print("{name} was revived!".format(name = self.name))
+
+    def knock_out(self):
+        # Knocking out a pokemon will flip its status to True.
+        self.is_knocked_out = True
+        # A knocked out pokemon can't have any health. This is a safety precaution. knock_out() should only be called if heath was set to 0, but if somehow the pokemon had health left, it gets set to 0.
+        if self.health != 0:
+            self.health = 0
+        print("{name} was knocked out!".format(name = self.name))
+
+    def lose_health(self, amount):
+        # Deducts heath from a pokemon and prints the current health remaining
+        self.health -= amount
+        if self.health <= 0:
+            #Makes sure the health doesn't become negative. Knocks out the pokemon.
+            self.health = 0
+            self.knock_out()
+        else:
+            print("{name} now has {health} health.".format(name = self.name, health = self.health))
+
+    def gain_health(self, amount):
+        # Adds to a pokemon's heath
+        # If a pokemon goes from 0 heath, then revive it
+        if self.health == 0:
+            self.revive()
+        self.health += amount
+        # Makes sure the heath does not go over the max health
+        if self.health >= self.max_health:
+            self.health = self.max_health
+        print("{name} now has {health} health.".format(name = self.name, health = self.health))
